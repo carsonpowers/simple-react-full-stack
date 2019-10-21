5cd79a55858e3eedee6ad8cd7933134c805bdf28
Carson Powers <carson.powers@gmail.com>
2019-10-20 19:08:24 -0500

    index.js clean up

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const os = require('os')
-
 const app = express()
 
 app.use(express.static('dist'))
-app.use(express.static('assets'))
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
-
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`))
